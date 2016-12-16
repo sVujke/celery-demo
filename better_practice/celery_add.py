4ea3f0de6c3cc0e66ab366bfce8f88cd7b9d5de3
vujke <stefanvujovic93@gmail.com>
2016-12-16 18:15:58 +0100

    fix

--- better_practice/celery_add.py
+++ better_practice/celery_add.py
@@ -5,6 +5,9 @@ import random
 def add(a, b):
     return a + b
 
-def add_listed(list):
-	for i in list:
-		add.delay(i, random.randint(0,10))
+def add_listed(listn):
+	for i in listn:
+		add.delay(i, random.randint(0,10)
+
+if __name__ == "__main__":
+    add_listed([5,6,7,8])
