f5f3b57062a7bf8651c6010538b4857fe8eeecf9
vujke <stefanvujovic93@gmail.com>
2016-12-16 17:54:42 +0100

    print py3

--- better_practice/celery_blog.py
+++ better_practice/celery_blog.py
@@ -4,7 +4,7 @@ from celery_config import app
 @app.task
 def fetch_url(url):
     resp = requests.get(url)
-    print resp.status_code
+    print(resp.status_code)
 
 def func(urls):
     for url in urls:
