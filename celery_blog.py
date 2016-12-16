7e2ad2db29c919378c48aab5c1ce582287575e10
vujke <stefanvujovic93@gmail.com>
2016-12-16 17:21:29 +0100

    celery_blog python3 edit

--- celery_blog.py
+++ celery_blog.py
@@ -5,7 +5,7 @@ app = Celery('celery_blog', broker='redis://localhost:6379/0')
 @app.task
 def fetch_url(url):
     resp = requests.get(url)
-    print resp.status_code
+    print(resp.status_code)
 
 def func(urls):
     for url in urls:
