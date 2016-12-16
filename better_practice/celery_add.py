0d2619b8d789dd014f0fc2082c4b5f9ea7480012
vujke <stefanvujovic93@gmail.com>
2016-12-16 18:29:53 +0100

    removed name and main

--- better_practice/celery_add.py
+++ better_practice/celery_add.py
@@ -5,9 +5,6 @@ import random
 def add(a, b):
     return a + b
 
-def add_listed(listn):
+def add_func(listn):
 	for i in listn:
 		add.delay(i, random.randint(0,10)
-
-if __name__ == "__main__":
-    add_listed([5,6,7,8])
